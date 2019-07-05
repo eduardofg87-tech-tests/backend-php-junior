f0f77d23e0d021efee341b50af6a9660963dec7a
Mathews Lima <mathews.limaa@gmail.com>
2019-07-05 08:53:18 -0300

    changes - put User Model inside the new directory called Models - change the namespace for all User Models references

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
