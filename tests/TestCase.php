e732c6ce8156df30f9c04d85bea9fabb1b5e16e3
Mathews Lima <mathews.limaa@gmail.com>
2019-07-05 12:58:29 -0300

    overrides the actingAs method for use Token JWT

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,28 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Overrides the default actingsAs method provided by Laravel
+     *
+     * @param User $user
+     */
+    public function actingAs(Authenticatable $user = null, $driver = null)
+    {
+        $user = $user ?? factory(User::class)->create();
+
+        $token = JWTAuth::fromUser($user);
+
+        $this->withHeader('Authorization', "Bearer {$token}");
+
+        return $this;
+    }
 }
