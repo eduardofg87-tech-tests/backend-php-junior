1f86b851184fda6299ba18d1a8f4adc15392783c
Adrysson <adrysson_santos@msn.com>
2019-07-07 14:06:15 -0300

    Criando seeder de usuários

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,8 +20,9 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'cpf' => $faker->cpf,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => 'secret',
         'remember_token' => Str::random(10),
     ];
 });
