32cc99b2018f9a4cd901d1300416c583907e05be
Adrysson <adrysson_santos@msn.com>
2019-07-08 00:38:20 -0300

    Iniciando casos de teste dos métodos do controller

---
+++ tests/Feature/UsersTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UsersTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testApiIndex()
+    {
+        $data = factory(User::class, 10)->create();
+        $response = $this->get('/api/users');
+        $response->assertStatus(200)
+            ->assertExactJson($data->toArray());
+    }
+
+    public function testApiShow()
+    {
+        $data = factory(User::class)->create();
+        $response = $this->get("/api/users/$data->id");
+        $response->assertStatus(200)
+            ->assertExactJson($data->toArray());
+    }
+
+}
