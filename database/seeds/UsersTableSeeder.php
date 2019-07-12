35b022b1a1ed42ea3ef61320955785acadb91fe1
Rodr1go <rodrigosc.pi@gmail.com>
2019-07-12 17:18:58 -0300

    adiciona seeder p/ criar usuario automaticamente

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::query()->truncate(); 
+        User::create([ 
+            'name' => 'Programador Backend PHP Júnior',
+            'cpf' => '11122233344',
+            'email' => 'admin@admin.com',
+            'password' => 'admin',
+        ]);
+    }
+}
